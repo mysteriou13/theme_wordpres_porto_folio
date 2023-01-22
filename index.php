f9f62fb6cb8fd83fbb5dca81f3d215b47c76b985
mysteriou <elio07@hotmail.fr>
2023-01-22 12:40:07 +0100

    ajout css au index

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang = "fr">
     <head>
-    
+  
     <title> massa anthony </title>
 
-        <link rel = "styleheet" href = "./style.css">
+    <?php
+
+
+    ?>        
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         
@@ -18,9 +24,11 @@
         </head>
 
     <body>
-        <header class = "d-flex justify-content-around flex-wrap">
+        <header class = "d-flex justify-content-around flex-wrap header">
 
-        <div> mon acceuil </div> <div> mes création </div> <div> contact</div> <div> présentation </div>
+        <div> <a href = ""> mon acceuil </a> </div> <div> <a  href = ""> mes création</a> </div> 
+        
+        <div> <a href = "">contact</a></div> <div> <a href = ""> présentation</a> </div>
 
             </header>
         
@@ -29,6 +37,7 @@
             </main>
 
                 <footer>
+                    footer
                     </footer>
         </body>
     </html>
