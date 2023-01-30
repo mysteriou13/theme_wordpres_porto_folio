f885dfd45917031e88810494cf870fb20a34ce3b
mysteriou <elio07@hotmail.fr>
2023-01-30 18:12:52 +0100

    ajout carrousel a la home page

--- head.php
+++ head.php
@@ -1,20 +1,18 @@
 
 <head>
-  
+
     <title> massa anthony </title>
-
-    <?php
-
-
-    ?>        
+     
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri()."/carrousel.css" ?>">
         
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
--- home_page.php
+++ home_page.php
@@ -2,14 +2,28 @@
 
 <div class = 'd-flex h-100'>
 
-<div class = "aside_box border border-dark">
-    aside
-</div>
+<div class="slider-container">
+   
+      <div>
+      <input class="slide-input" id="slide-dot-1" for="slide-dot-1" type="radio" name="slides" checked>ff
+      <img class="slide-img" src="https://www.codeur.com/tuto/wp-content/uploads/2021/12/slide1.jpg">
 
-<div class = "home_box border border-dark">
-    <?php 
-    require_once(get_template_directory()."/page/creation.php");
-    ?>
-</div>
+      <input class="slide-input" id="slide-dot-2" type="radio" name="slides">
+      <img class="slide-img" src="https://www.codeur.com/tuto/wp-content/uploads/2021/12/slide2.jpg">
+      
+      <input class="slide-input" id="slide-dot-3" type="radio" name="slides">
+      <img class="slide-img" src="https://www.codeur.com/tuto/wp-content/uploads/2021/12/slide3.jpg">
+  </div>
 
+    </div>
+
+
+    <div class="menu">
+    
+    <label for="slide-dot-1"></label>
+    <label for="slide-dot-2"></label>
+    <label for="slide-dot-3"></label>
+  </div>
+  
+  
 </div>
